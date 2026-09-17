4ec384f1cbc0c8bffa45ea6bf4750b4c9a0b45b9
a_c <peacemakerallones@gmail.com>
2026-09-18 04:12:51 +0800

    fix import in login

--- swag_labs_ui/tests/test_login.py
+++ swag_labs_ui/tests/test_login.py
@@ -1,6 +1,6 @@
 import pytest
 from playwright.sync_api import Page
-from pages.login_page import LoginPage
+from swag_labs_ui.pages.login_page import LoginPage
 
 USERS_TO_TEST = [
     "standard_user",
